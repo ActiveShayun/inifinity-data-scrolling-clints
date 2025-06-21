0d2334efb967c7e75dd6212c595ed841766b3f36
ActiveShayun <wevdeveloper0477gmail.com>
2025-06-21 18:04:52 +0600

    varcel api link add

--- src/hooks/AxiosPublic.jsx
+++ src/hooks/AxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const instant = axios.create({
-    baseURL: 'https://infinitscroll-api-server.onrender.com',
+    baseURL: 'https://infinitscroll-api-server-1.onrender.com',
     withCredentials: true
 })
 
